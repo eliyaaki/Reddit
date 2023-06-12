a031ce24aeece7ab21228a4c8ea3fb7317afb13a
eliyaaki <69030616+eliyaaki@users.noreply.github.com>
2023-06-12 15:44:26 +0300

    Adding Swagger-ui to the application

--- src/main/java/com/joyned/reddit/config/Config.java
+++ src/main/java/com/joyned/reddit/config/Config.java
@@ -1,5 +1,8 @@
 package com.joyned.reddit.config;
 
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +10,14 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class Config {
+    @Bean
+    public OpenAPI usersSwaggerOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Reddit Swagger")
+                        .description("\n" +
+                                "Reddit Swagger is an integration of the Swagger framework into the Reddit application, providing a user-friendly interface to explore and interact with the application's RESTful APIs.")
+                        .version("1.0"));
+    }
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
